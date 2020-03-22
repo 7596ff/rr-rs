9db26fb676f7a048a467cd043f89cdfbc798c006
Cassandra McCarthy <cassie@7596ff.com>
2020-03-22 15:28:21 -0400

    edit ping command to handle with new util function

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,14 +1,24 @@
 use serenity::{client::Context, model::channel::Message, Error};
+use crate::util;
 
-pub fn ping(ctx: &Context, msg: &Message) -> Result<Message, Error> {
+pub fn ping(ctx: &Context, msg: &Message) -> Result<(), Error> {
     match msg.channel_id.say(&ctx, "pong!") {
         Ok(mut sent) => {
             let latency = sent.timestamp.timestamp_millis() - msg.timestamp.timestamp_millis();
-            sent.edit(ctx, |m| {
+
+            let result = sent.edit(ctx, |m| {
                 m.content(format!("🏓 Message send latency: {} ms", latency))
-            })?;
-            Ok(sent)
-        }
-        Err(why) => Err(why),
+            });
+
+            // strange matching based on how serenity handles edited messages
+            // will be cleaner when i move to dawn
+            match result {
+                Ok(()) => util::handle_sent_message(&msg, Ok(sent), "ping"),
+                Err(why) => util::handle_sent_message(&msg, Err(why), "ping"),
+            }
+        },
+        Err(why) => util::handle_sent_message(&msg, Err(why), "ping"),
     }
+
+    Ok(())
 }
